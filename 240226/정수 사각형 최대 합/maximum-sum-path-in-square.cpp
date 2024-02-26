ca1c1efc7c7f556660e0647b300aef3c7c495608
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-02-26 16:50:11 +0900

    Title: 정수 사각형 최대 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240226/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 240226/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -1,30 +1,36 @@
-#include <iostream>
-using namespace std;
 int map[100][100];
 long long dp[100][100];
 int n;
-void solve(){
-    dp[1][1] = map[1][1];
+
+void solve() {
+
+    dp[0][0] = map[0][0];
+
+
     for(int i = 1; i < n; i++) {
         dp[i][0] = dp[i-1][0] + map[i][0]; // 첫 열
         dp[0][i] = dp[0][i-1] + map[0][i]; // 첫 행
     }
-     for(int i = 1; i < n; i++) {
+
+
+    for(int i = 1; i < n; i++) {
         for(int j = 1; j < n; j++) {
             dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + map[i][j];
         }
     }
 
-    // (N, N) 위치의 최대 합 출력
+
     cout << dp[n-1][n-1] << endl;
 }
+
 int main() {
     cin >> n;
-    for(int i =1 ;i<=n;i++){
-        for(int j = 1;j<=n;j++){
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
             cin >> map[i][j];
         }
     }
+
     solve();
     return 0;
 }
