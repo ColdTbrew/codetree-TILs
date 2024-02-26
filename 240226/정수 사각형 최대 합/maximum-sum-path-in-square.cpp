2622143ba4a3837d95390f9c8f5f5ed191715ef2
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-02-26 16:47:11 +0900

    Title: 정수 사각형 최대 합, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240226/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 240226/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -5,6 +5,10 @@ long long dp[100][100];
 int n;
 void solve(){
     dp[1][1] = map[1][1];
+    for(int i = 1; i < n; i++) {
+        dp[i][0] = dp[i-1][0] + map[i][0]; // 첫 열
+        dp[0][i] = dp[0][i-1] + map[0][i]; // 첫 행
+    }
     for(int i =1 ;i<=n;i++){
         for(int j = 1;j<= n;j++){
             dp[i][j] = max(dp[i-1][j] + map[i][j],dp[i][j-1] + map[i][j]);
