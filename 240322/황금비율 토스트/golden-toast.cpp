d7fe2133f372c866d5fc35f85ddcb635153ec080
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-03-22 21:31:10 +0900

    Title: 황금비율 토스트, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240322/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    list<char> l;
+    list<char>::iterator it;
+
+    int n, m;
+    cin >> n >> m;
+    while(n--){
+        char c;
+        cin >> c;
+        l.push_back(c);
+    }
+    it = l.end();
+    while(m--){
+        char input;
+        cin >> input;
+        if(input =='L'){
+            it--;
+        }
+        if(input =='R'){
+            it++;
+        }
+        if(input =='D'){
+            l.erase(it);
+        }
+        if(input =='P'){
+            char x;
+            cin >> x;
+            l.insert(it, x);
+        }
+    }
+    for(it = l.begin(); it != l.end(); it++) {
+        cout << *it;      
+    }   
+
+    return 0;
+}
