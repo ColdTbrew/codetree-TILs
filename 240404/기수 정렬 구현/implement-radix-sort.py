0835aa6b960d40653bb6211ce001f0b47b57ee56
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-04-04 22:40:10 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240404/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def radix_sort(nums):
+    max_num = max(nums)
+    digit = 0
+    while max_num >= 10**digit:
+        buckets =[deque() for _ in range(10)]
+        for num in nums:
+            digit_value = num //(10**digit)
+            buckets[digit_value].append(num)
+        new_nums = []
+        for bucket in buckets:
+            new_nums.extend(bucket)
+
+        digit += 1
+
+    return new_nums
+
+n = int(input())
+nums = list(map(int, input().split(' ')))
+
+sorted_nums = radix_sort(nums)
+print(*sorted_nums)
