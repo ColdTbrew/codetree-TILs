70bea969a4247872fd1f90e20154ce4592b93543
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-09-25 17:26:38 +0900

    Title: 삼각형 컨베이어 벨트, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,36 @@
+n, t = list(map(int, input().split()))
+
+belt = []
+
+for i in range(n):
+    row = list(map(int, input().split()))
+    belt.append(row)
+
+flatten_belt = []
+for i in range(n):
+    for j in range(n):
+        flatten_belt.append(belt[i][j])
+
+
+def rotation():
+    temp = flatten_belt[n*n-1]
+    for i in range(n*n-1, 0, -1):
+        flatten_belt[i] = flatten_belt[i-1] 
+    flatten_belt[0] = temp
+# print("before")
+# for i in range(n*n):
+#     print(flatten_belt[i])
+
+for rot in range(t):
+    rotation()
+# print("after")
+# for i in range(n*n):
+#     print(flatten_belt[i])
+
+#printing
+count = 0
+for i in range(n):
+    for j in range(n):
+        print(flatten_belt[count], end=' ')
+        count+= 1
+    print()
