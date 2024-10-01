88320b0cf03de10d74da9cbe98e4db2521232d39
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-01 22:20:34 +0900

    Title: xor 결과 최대 만들기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/xor 결과 최대 만들기/max-of-xor.py
+++ 241001/xor 결과 최대 만들기/max-of-xor.py
@@ -7,7 +7,7 @@ ans = list()
 def choose(cur_num):
     global max_res
     if cur_num == m+1:
-        a = 1
+        a = 0
         for r in ans:
             a ^= r
         max_res = max(max_res, a)
