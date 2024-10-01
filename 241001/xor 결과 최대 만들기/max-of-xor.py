4e333bd3b9be83a67d0ded62f5aeca07e6ddce94
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-01 22:24:06 +0900

    Title: xor 결과 최대 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241001/xor 결과 최대 만들기/max-of-xor.py
+++ 241001/xor 결과 최대 만들기/max-of-xor.py
@@ -4,19 +4,17 @@ li = list(map(int, input().split()))
 max_res = 0
 
 ans = list()
-def choose(cur_num):
+def choose(cur_num, start_idx):
     global max_res
     if cur_num == m:
-        a = 0
+        a = 0  # XOR 초기값은 0이어야 함
         for r in ans:
             a ^= r
         max_res = max(max_res, a)
         return
-    if cur_num > m:
-        return 
-    for elem in li:
-        ans.append(elem)
-        choose(cur_num+1)
+    for i in range(start_idx, len(li)):  # start_idx를 이용하여 중복 선택 방지
+        ans.append(li[i])
+        choose(cur_num+1, i+1)  # i+1을 넘겨서 중복 선택을 방지
         ans.pop()
 
 choose(0)
