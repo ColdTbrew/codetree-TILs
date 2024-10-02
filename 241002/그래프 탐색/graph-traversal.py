21ed1ec2b0f289a5411878376b25b8254dd9628f
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-02 15:59:55 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241002/그래프 탐색/graph-traversal.py
+++ 241002/그래프 탐색/graph-traversal.py
@@ -3,20 +3,20 @@
 #노드개수만큼의 배열에 번호를 적어놔야함
 
 n, m = tuple(map(int, input().split()))
-mat = [[0 for _ in range(n)] for _ in range(n)]
-visited = [False for _ in range(n)]
+mat = [[0 for _ in range(n+1)] for _ in range(n+!)]
+visited = [False for _ in range(n+1)]
 for i in range(m):
     x, y = tuple(map(int, input().split()))
-    mat[x-1][y-1] = 1
-    mat[y-1][x-1] = 1
+    mat[x][y] = 1
+    mat[y][x] = 1
 
 count = 0
 def dfs(node):
     global count
     for cur_node in range(1, n+1):
-        if visited[cur_node-1] != True and mat[node-1][cur_node-1]:
+        if visited[cur_node] != True and mat[node][cur_node]:
             #print(cur_node)
-            visited[cur_node-1] = True
+            visited[cur_node] = True
             count += 1
             dfs(cur_node)
 
@@ -26,5 +26,5 @@ def dfs(node):
 #     for j in range(n):
 #         print(mat[i][j], end = " ")
 #     print()
-dfs(0)
+dfs(1)
 print(count)
