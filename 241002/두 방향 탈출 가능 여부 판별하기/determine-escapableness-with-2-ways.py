7f4343a9135cab94c1febcea78a098488e5ef811
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-02 16:26:29 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,31 @@
+#각 행에 뱀이 없는 경우 1, 뱀이 있는 경우 0 
+# 1인 곳만 갈 수 있음
+n, m = tuple(map(int, input().split()))
+start = (0,0)
+end = (n,m)
+suc = False
+mat = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    mat.append(row)
+
+
+def cango(x, y):
+    return 0 <= x < n and 0 <= y < n and mat[x][y] == 1 and not visited[x][y]
+
+def dfs(x, y):
+    dx = [1, 0]
+    dyx = [0, 1]
+    for dx, dy in dxs, dys:
+        newx , newy = dx + x, dy+ y
+        if cango(newx, newy):
+            visited[newx][newy] = True
+            if (newx, newy) == end:
+                suc = True
+            dfs(newx, newy)
+
+
+if suc:
+    print(1)
+else:
+    print(0)
