78dcbe4037a4b1536d0bffa2573f3b84dc4b1795
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-04 16:07:51 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241004/피보나치 수/fibonacci-number.py
+++ 241004/피보나치 수/fibonacci-number.py
@@ -2,13 +2,21 @@ n = int(input())
 
 # topdown 메모이제이션
 memo = [-1 for _ in range(n+1)]
-def fibo(x):
-    if memo[x] != -1:
-        return memo[x]
-    if x <= 2:
-        memo[x] = 1
-    else:
-        memo[x] = fibo(x-1)+fibo(x-2)
-    return memo[x]
+# def fibo(x):
+#     if memo[x] != -1:
+#         return memo[x]
+#     if x <= 2:
+#         memo[x] = 1
+#     else:
+#         memo[x] = fibo(x-1)+fibo(x-2)
+#     return memo[x]
 
-print(fibo(n))
+# print(fibo(n))
+
+# bottom up tabulation
+f = [-1 for _ in range(n+1)]
+f[1] = 1
+f[2] = 1
+for i in range(3, n+1):
+    f[i] = f[i-1]+f[i-2]
+print(f[n])
