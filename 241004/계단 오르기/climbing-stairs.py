77b77f6ec4647ea5008b865269e58be64854a4d8
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-04 16:32:25 +0900

    Title: 계단 오르기, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241004/계단 오르기/climbing-stairs.py
@@ -0,0 +1,16 @@
+ans = -1
+n= int(input())
+
+f = [[-1] for _ in range(10000)]
+f[1] = 0
+f[2] = 1
+f[3] = 1
+f[4] = 0
+f[5] = 2
+for i in range(6, n+1):
+    f[i] = f[i-3] + f[i-2]
+
+if f[n]:
+    print(f[n]%10007)
+else:
+    print(0)
