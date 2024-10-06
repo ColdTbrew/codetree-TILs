ab36020a887f5ec418cb8947380ccb5053d21357
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-06 20:32:57 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241006/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -0,0 +1,12 @@
+#dp[i]=max(dp[i−1]+a[i],2a[i])
+# 마지막 원소가 i라 했을때 얻을 수 있는 최대 점수
+# 이전 dp값 + a[i](새로운 연속부분 더하기) , 현재a[i]값의 2배 중 큰 값을 dp[i]에 넣을 거임
+
+n = int(input())
+mat = list(map(int, input().split()))
+dp = [-1]*(n+1)
+dp[0] = mat[0]
+for i in range(1, n+1):
+    dp[i] = max(dp[i-1] + mat[i-1], 0)
+
+print(dp[n])
