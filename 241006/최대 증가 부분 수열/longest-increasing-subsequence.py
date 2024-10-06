c595b01d67832732606d3b92f0d5d5d5d768c0dc
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-06 15:53:07 +0900

    Title: 최대 증가 부분 수열, Time: 256ms, Memory: 25MB, Status: Passed - CodeTree

--- 241006/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 241006/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,12 +1,10 @@
 n = int(input())
-li = list(input().split())
+li = list(map(int, input().split()))
 
 dp = [1] * n
 
 for i in range(1, n):
     for j in range(0, i):
-        if dp[j] == 1:
-            continue
         if li[j] < li[i]: # i자리의 전에 해당하는 부분에 값이 더 작다면 dp 갱신
             dp[i] = max(dp[i], dp[j] + 1)
 
