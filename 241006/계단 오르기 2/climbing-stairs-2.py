48d3ccf533e2ff8cfd722257b66a71cd63fbfde0
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-06 21:51:47 +0900

    Title: 계단 오르기 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241006/계단 오르기 2/climbing-stairs-2.py
+++ 241006/계단 오르기 2/climbing-stairs-2.py
@@ -8,12 +8,12 @@ for i in range(1, n+1):
     if i >= 1:
         for j in range(1, 4):  # 1계단은 최대 3번까지 가능
             if dp[i-1][j-1] != -1:  # 이전 층에서 j-1번 1계단을 사용한 경우
-                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + coins[i-1])
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + a[i-1])
     
     # 2계단 올라오는 경우
     if i >= 2:
         if dp[i-2][0] != -1:  # 2계단을 오르는 경우 1계단 사용 횟수는 0으로 유지
-            dp[i][0] = max(dp[i][0], dp[i-2][0] + coins[i-1])
+            dp[i][0] = max(dp[i][0], dp[i-2][0] + a[i-1])
 
 
 print(max(dp[n]))
