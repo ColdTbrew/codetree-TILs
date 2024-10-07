84c1b247a09da5743e057c6f5e1009034173189c
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-07 13:06:34 +0900

    Title: 숫자 전쟁, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241007/숫자 전쟁/number-war.py
+++ 241007/숫자 전쟁/number-war.py
@@ -9,11 +9,11 @@ for i in range(n-1, -1, -1):
         # 남우의 카드가 상대방의 카드보다 작을 때
         if me[j] < ops[i]:
             dp[i][j] = max(dp[i][j], dp[i][j+1] + me[j])
-        # 카드가 같을 때 (점수 없음)
-        elif me[j] == ops[i]:
-            dp[i][j] = max(dp[i][j], dp[i+1][j+1])
-        # 남우의 카드가 더 클 때는 카드만 버림 (점수 없음)
+        # 남우의 카드가 같거나 클 때
         else:
+            # 남우의 카드를 버리는 경우
             dp[i][j] = max(dp[i][j], dp[i+1][j])
+            # 상대방의 카드를 버리는 경우
+            dp[i][j] = max(dp[i][j], dp[i+1][j+1])
 
 print(dp[0][0])
