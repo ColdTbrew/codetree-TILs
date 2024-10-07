b2e2846ec707cfb31855ecd1d911601bd4cbc0dd
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-10-07 13:24:36 +0900

    Title: 숫자 전쟁, Time: 188ms, Memory: 29MB, Status: Passed - CodeTree

--- 241007/숫자 전쟁/number-war.py
+++ 241007/숫자 전쟁/number-war.py
@@ -2,18 +2,21 @@
 n = int(input())
 ops = list(map(int, input().split())) 
 me = list(map(int, input().split())) 
-dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
 
-for i in range(n-1, -1, -1):
-    for j in range(n-1, -1, -1):
-        # 남우의 카드가 상대방의 카드보다 작을 때
+dp[0][0] = 0
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == -1:
+            continue
+        if ops[i] < me[j]:
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j])
         if me[j] < ops[i]:
-            dp[i][j] = max(dp[i][j], dp[i][j+1] + me[j])
-        elif me[j] > ops[i]: #i가 아래에서 올라옴
-            dp[i][j] = max(dp[i][j], dp[i+1][j])
-        else:
-            dp[i][j] = max(dp[i][j], dp[i+1][j+1])    
-        #둘다 버리는 경우가 클경우
-        dp[i][j] = max(dp[i][j], dp[i+1][j+1])
+            dp[i][j+1] = max(dp[i][j+1] , dp[i][j]+me[j])
+        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j])
+ans = 0
+for i in range(n+1):
+    ans = max(ans, dp[n][i])
+    ans = max(ans, dp[i][n])
 
-print(dp[0][0])
+print(ans)
