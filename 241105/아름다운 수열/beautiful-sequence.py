6dbfc99587a1534f8fe459cbfdf1c7e3d8d2ae1e
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-11-05 01:22:45 +0900

    Title: 아름다운 수열, Time: 75ms, Memory: 17MB, Status: Passed - CodeTree

--- 241105/아름다운 수열/beautiful-sequence.py
+++ 241105/아름다운 수열/beautiful-sequence.py
@@ -14,15 +14,17 @@ ans = []
 
 # a 리스트에서 길이가 m인 수열을 가져와서 아름다운 수열인지 검사해야함
 for start_idx in range(0, N-M+1):
-    hubo = a[start_idx : start_idx+M]
-    for hupo_permu in permutations(hubo, M):     
-        subs = []
-        for x, y in zip(hupo_permu, b):
-            sub = x - y
-            subs.append(sub)
-        if all(x == subs[0] for x in subs):
-            ans.append(start_idx+1)
-            break
+    # print("start_", start_idx)
+    hubo = sorted(a[start_idx : start_idx+M])
+    # print(hubo)
+    b.sort()
+    # print(b)
+    subs = []
+    for x, y in zip(hubo, b):
+        sub = x - y
+        subs.append(sub)
+    if all(x == subs[0] for x in subs):
+        ans.append(start_idx+1)
 
 print(len(ans))
 for a in ans:
