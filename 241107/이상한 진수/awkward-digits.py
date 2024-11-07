42cc5cf028e590757c975e32e04f3c50279b07d3
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-11-07 18:42:40 +0900

    Title: 이상한 진수, Time: 5081ms, Memory: 19MB, Status: Time Limit Exceed - CodeTree

--- 241107/이상한 진수/awkward-digits.py
+++ 241107/이상한 진수/awkward-digits.py
@@ -1,15 +1,29 @@
-a = str(input()) #2진법인풋
-b = str(input()) #3진법 인풋
+def is_one_bit_diff(x, y):
+    """ 두 숫자 x와 y가 이진수에서 한 비트만 다른지 확인 """
+    return bin(x ^ y).count('1') == 1
 
-N_a = 0
-N_b = 0
-two = 2
-for idx in range(len(str(a))):
-    if a[idx]:
-        N_a += 2 ** idx
+# 입력 받기
+a = input().strip()  # 2진법 입력
+b = input().strip()  # 3진법 입력
 
-for idx in range(len(str(b))):
-    if b[idx]:
-        N_b += 3 ** idx
+# a와 b의 길이를 고려하여 최대 N을 설정합니다.
+max_value = 1_000_000_000
 
-print((N_a+N_b)//2)
+# 가능한 N을 찾기 위해 0부터 max_value까지 검사
+for N in range(max_value + 1):
+    # N을 2진법과 3진법으로 변환
+    binary_N = bin(N)[2:]  # 2진법 문자열 (앞의 '0b' 제거)
+    ternary_N = ''
+    
+    temp = N
+    while temp > 0:
+        ternary_N = str(temp % 3) + ternary_N
+        temp //= 3
+    if not ternary_N:  # N이 0일 경우
+        ternary_N = '0'
+    
+    # 2진법과 3진법에서 각각 a, b와 정확히 한 비트만 다르면
+    if len(binary_N) == len(a) and len(ternary_N) == len(b):
+        if is_one_bit_diff(int(binary_N, 2), int(a, 2)) and is_one_bit_diff(int(ternary_N, 3), int(b, 3)):
+            print(N)
+            break  # 유효한 N을 찾으면 종료
