b70e3f627332ad3a731a493aed11dad5952a0fe3
Seunghyuk Choi <117180508+ColdTbrew@users.noreply.github.com>
2024-11-07 18:11:40 +0900

    Title: 숫자 분석, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241107/숫자 분석/number-analysis.py
+++ 241107/숫자 분석/number-analysis.py
@@ -1,8 +1,13 @@
-original = input()
-s = list(original)
-summ = 0
-for i in s:
+original = input()  # 사용자 입력
+s = list(original)  # 입력된 문자열을 리스트로 변환
+summ = 0  # 합계를 저장할 변수 초기화
+
+# 각 문자에 대해 정수로 변환하여 합계 계산
+for i in s:  # s에서 반복
     summ += int(i)
-s.reverse()
-ans = ''.join(map(str, s))
+
+s.reverse()  # 리스트를 반대로 뒤집음
+ans = ''.join(s)  # 뒤집힌 리스트를 문자열로 변환
+
+# 결과 출력
 print(ans, summ)
